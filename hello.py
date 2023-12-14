efe37fc7808696f0865e76e1fe4fa4b514e48932
root <root@LAPTOP-T90E3EO0.localdomain>
2023-12-14 22:16:19 +0000

    converted to API using Flask

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Server works!'
+
+@app.route('/hello')
+def say_hello():
+    return 'Hello World!'
