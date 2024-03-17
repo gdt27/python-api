88c502b47c772b92c37fbb0a577f9849a0816bfa
cousinmose27 <gd.timms@googlemail.com>
2024-03-17 23:39:02 +0000

    added container service deployment

--- modules/lightsail/main.tf
+++ modules/lightsail/main.tf
@@ -20,3 +20,30 @@ resource "aws_lightsail_container_service" "my_container_service" {
   scale       = 1
   is_disabled = false
 }
+
+resource "aws_lightsail_container_service_deployment_version" "my_container_service_deployment" {
+  container {
+    container_name = "python-api"
+    image          = ":python-api.python-api.1"
+
+    ports = {
+      5000 = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "python-api"
+    container_port = 5000
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
+  }
+
+  service_name = aws_lightsail_container_service.my_container_service.name
+}
